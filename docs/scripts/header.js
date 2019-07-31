08540992eeb6222be17b3360c0130a5373fa40e2
plamenpik <plamen_pik@abv.bg>
2019-07-31 23:23:46 +0300

    Header rework, fixed logo img, scripts and animation added

---
+++ docs/scripts/header.js
@@ -0,0 +1,19 @@
+const classArray = ["image-one", "image-two", "image-three"];
+        const textArray = ["Осветление", "Ел. Инсталации", "Коледна украса"];
+        let classCount = 0;
+       
+        function classChanger() {
+            if (classCount == classArray.length) {
+                classCount = 0;
+            }
+            document.getElementById("header-images").classList = classArray[classCount];
+            document.getElementById("header-text").textContent = textArray[classCount];
+            classCount++;
+            setTimeout("classChanger()", 7000);
+        }
+
+        function switchImage(count) {
+            classCount = count + 1;
+            document.getElementById("header-images").classList = classArray[count];
+            document.getElementById("header-text").textContent = textArray[count];
+        }
