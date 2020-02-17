fa2f02652303287da8c84387aca252a1928fe308
plamenpik <plamen_pik@abv.bg>
2020-02-17 19:56:37 +0200

    Header section refactoring

--- scripts/header.js
+++ scripts/header.js
@@ -1,21 +1,27 @@
-const classArray = ["image-one", "image-two", "image-three"];
-        const textArray = ["Осветление", "Ел. Инсталации", "Коледна украса"];
-        let classCount = 0;
-       
-        function classChanger() {
-            if (classCount == classArray.length) {
-                classCount = 0;
-            }
-            document.getElementById("header-images").classList = classArray[classCount];
-            document.getElementById("switcher").classList = classArray[classCount];
-            document.getElementById("header-text").textContent = textArray[classCount];
-            classCount++;
-            setTimeout("classChanger()", 7000);
-        }
+const classArray = ['image-one', 'image-two', 'image-three'];
+const textArray = ['Осветление', 'Ел. Инсталации', 'Коледна украса'];
+let classCount = 0;
+let headerImages = document.getElementById('header-images');
+let switcher = document.getElementById('switcher');
+let headerText = document.getElementById('header-text');
 
-        function switchImage(count) {
-            classCount = count + 1;
-            document.getElementById("header-images").classList = classArray[count];
-            document.getElementById("switcher").classList = classArray[count];
-            document.getElementById("header-text").textContent = textArray[count];
-        }
+
+function classChanger() {
+    if (classCount == classArray.length) {
+        classCount = 0;
+	}
+	setClass(classCount);
+    classCount++;
+    setTimeout('classChanger()', 6000);
+}
+
+function switchImage(count) {
+    setClass(count);
+    classCount = count + 1;
+}
+
+function setClass(number) {
+    headerImages.classList = classArray[number];
+	switcher.classList = classArray[number];
+	headerText.textContent = textArray[number];
+}
